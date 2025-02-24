03f2902d1516c502cdf023c408c1a48f4a406bd1
nkasenides <hfnovember@hotmail.com>
2025-02-25 00:37:28 +0200

    Class inspection code

--- firestorm_flutter/lib/gen/firestorm_inspector.dart
+++ firestorm_flutter/lib/gen/firestorm_inspector.dart
@@ -9,58 +9,63 @@ class FirestormGenerator extends GeneratorForAnnotation<FirestormObject> {
   @override
   dynamic generateForAnnotatedElement(Element element, ConstantReader annotation, BuildStep buildStep) {
     print('Found annotated element: ${element.name}');
-    return "int hi = 0;";
     // ...
     // Make sure the element is a ClassElement
-//     if (element is! ClassElement) {
-//       return '// Not a class.';
-//     }
+    if (element is! ClassElement) {
+      return '// Not a class.';
+    }
+
+    final classElement = element;
+    final className = classElement.displayName;
+
+    // Gather details
+    final fieldsInfo = classElement.fields
+    // Filter out synthetic fields (like getters/setters)
+        .where((f) => !f.isSynthetic)
+        .map((f) {
+      final fieldName = f.displayName;
+      final fieldType = f.type.getDisplayString(withNullability: false);
+      return '//Field: $fieldName, Type: $fieldType';
+    })
+        .join('\n  ');
+
+    classElement.allSupertypes.forEach((supertype) {
+      if (!supertype.isDartCoreObject) {
+        print("Supertype: " + supertype.getDisplayString(withNullability: false));
+      }
+    });
+
+    // Constructors (just to show example)
+    final constructorInfo = classElement.constructors.map((c) {
+      final params = c.parameters.map((p) {
+        final paramType = p.type.getDisplayString(withNullability: false);
+        return '${p.name}: $paramType';
+      }).join(', ');
+      return 'Constructor ${c.name.isEmpty ? '(default)' : c.name}($params)';
+    }).join('\n  ');
+
+    // Methods (just to show example)
+    final methodInfo = classElement.methods
+        .where((m) => !m.isSynthetic)
+        .map((m) {
+      final returnType = m.returnType.getDisplayString(withNullability: false);
+      return 'Method: ${m.displayName}, returns $returnType';
+    })
+        .join('\n  ');
+
+    print("Hello there!");
+
+    // Generate a string (could also be code to define a helper class, etc.)
+    return '''
+// Inspecting class: $className
+// Fields:
+//   $fieldsInfo
 //
-//     final classElement = element;
-//     final className = classElement.displayName;
+// Constructors:
+//   $constructorInfo
 //
-//     // Gather details
-//     final fieldsInfo = classElement.fields
-//     // Filter out synthetic fields (like getters/setters)
-//         .where((f) => !f.isSynthetic)
-//         .map((f) {
-//       final fieldName = f.displayName;
-//       final fieldType = f.type.getDisplayString(withNullability: false);
-//       return 'Field: $fieldName, Type: $fieldType';
-//     })
-//         .join('\n  ');
-//
-//     // Constructors (just to show example)
-//     final constructorInfo = classElement.constructors.map((c) {
-//       final params = c.parameters.map((p) {
-//         final paramType = p.type.getDisplayString(withNullability: false);
-//         return '${p.name}: $paramType';
-//       }).join(', ');
-//       return 'Constructor ${c.name.isEmpty ? '(default)' : c.name}($params)';
-//     }).join('\n  ');
-//
-//     // Methods (just to show example)
-//     final methodInfo = classElement.methods
-//         .where((m) => !m.isSynthetic)
-//         .map((m) {
-//       final returnType = m.returnType.getDisplayString(withNullability: false);
-//       return 'Method: ${m.displayName}, returns $returnType';
-//     })
-//         .join('\n  ');
-//
-//     print("Hello there!");
-//
-//     // Generate a string (could also be code to define a helper class, etc.)
-//     return '''
-// // Inspecting class: $className
-// // Fields:
-// //   $fieldsInfo
-// //
-// // Constructors:
-// //   $constructorInfo
-// //
-// // Methods:
-// //   $methodInfo
-// ''';
+// Methods:
+//   $methodInfo
+''';
   }
 }
---
+++ flutter_test_app/lib/student.dart
@@ -0,0 +1,12 @@
+import 'package:firestorm/annotation/firestorm_object.dart';
+import 'package:flutter_test_app/person.dart';
+
+@FirestormObject()
+class Student extends Person {
+
+  String studentID;
+  String school;
+
+  Student(super.id, super.firstname, super.lastname, super.age, super.height, super.isEmployed, super.friends, this.studentID, this.school);
+
+}
