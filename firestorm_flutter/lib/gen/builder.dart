02892afd6f5a8990e652561513b460eb045b4e91
nkasenides <hfnovember@hotmail.com>
2025-03-15 01:20:44 +0200

    New Builders.

---
+++ firestorm_flutter/lib/gen/builder.dart
@@ -0,0 +1,4 @@
+import 'package:build/build.dart';
+import 'firestorm_builder.dart';
+
+Builder firestormBuilder(BuilderOptions options) => FirestormBuilder();
--- firestorm_flutter/lib/gen/firestorm_builder.dart
+++ firestorm_flutter/lib/gen/firestorm_builder.dart
@@ -1,12 +1,42 @@
-// lib/src/firestorm_builder.dart
+import 'dart:async';
 import 'package:build/build.dart';
-import 'package:firestorm/gen/firestorm_inspector.dart';
-import 'package:firestorm/gen/generator_for_all_elements.dart';
 import 'package:source_gen/source_gen.dart';
+import 'package:glob/glob.dart';
 
-/// A top-level function that creates your builder.
-Builder firestormBuilder(BuilderOptions options) =>
-    LibraryBuilder(
-      FirestormGenerator(),
-      generatedExtension: '.g.dart',
-    );
+class FirestormBuilder implements Builder {
+
+  @override
+  final buildExtensions = const {
+    r'$lib$': ['generated/firestorm_models.dart'],
+  };
+
+  @override
+  Future<void> build(BuildStep buildStep) async {
+    final buffer = StringBuffer();
+    buffer.writeln('// GENERATED CODE - DO NOT MODIFY BY HAND\n');
+    buffer.writeln('class CombinedRegistry {');
+
+    await for (final input in buildStep.findAssets(Glob('lib/**.dart'))) {
+      if (!await buildStep.resolver.isLibrary(input)) continue;
+      final libraryElement = await buildStep.resolver.libraryFor(input);
+      final libraryReader = LibraryReader(libraryElement);
+
+      for (final clazz in libraryReader.classes) {
+        if (clazz.metadata.any((m) => m.element?.displayName == 'FirestormObject')) {
+          buffer.writeln('  static const String ${clazz.name} = \'${clazz.name}\';');
+        }
+      }
+    }
+
+    buffer.writeln('}');
+
+    // Optional formatting:
+    // final formatter = DartFormatter();
+    // final formattedOutput = DartFormatter().format(buffer.toString());
+
+    final formattedOutput = buffer.toString();
+
+    final outputId = AssetId(buildStep.inputId.package, 'lib/generated/firestorm_models.dart');
+    await buildStep.writeAsString(outputId, formattedOutput);
+  }
+}
