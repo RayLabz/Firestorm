b5d5d7643d23d5350eb9e1f03163a5c869b4ae75
nkasenides <hfnovember@hotmail.com>
2025-07-05 20:13:35 +0300

    Demonstrator app

---
+++ firestorm_demonstrator/android/app/src/main/java/com/raylabz/firestorm/demonstrator/firestorm_demonstrator/MainActivity.java
@@ -0,0 +1,6 @@
+package com.raylabz.firestorm.demonstrator.firestorm_demonstrator;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ firestorm_demonstrator/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:firestorm/fs/fs.dart';
+import 'package:firestorm/rdb/rdb.dart';
+import 'package:flutter/material.dart';
+
+void main () async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // await FS.init();
+  // await RDB.init();
+  // registerClasses();
+
+  runApp(Container());
+}
